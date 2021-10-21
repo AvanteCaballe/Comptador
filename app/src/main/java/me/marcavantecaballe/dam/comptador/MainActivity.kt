67e6d1395d65a1d8f779323e1145264f250e9a40
AvanteCaballe <marcavante@iesebre.com>
2021-10-21 16:17:01 +0200

    Afegit Log.D

--- app/src/main/java/me/marcavantecaballe/dam/comptador/MainActivity.kt
+++ app/src/main/java/me/marcavantecaballe/dam/comptador/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -11,6 +12,9 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     private val INITIAL_TIME = 60;
+
+    private val TAG = MainActivity::class.java.simpleName;
+
     internal lateinit var tapMeButton: Button
     internal lateinit var timeTextView: TextView
     internal lateinit var counterTextView: TextView
@@ -23,8 +27,13 @@ class MainActivity : AppCompatActivity() {
     internal val intervalCountDownTimer : Long = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"Hola mon! onCreate");
+        Log.d(TAG, counter.toString())
+        Log.d(TAG, time.toString());
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         initCountdown()
 
